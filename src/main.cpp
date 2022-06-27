2a3a7ee5da2db49c024716adb99fe911d8df7a9c
haris-sabotic <haris.sinfo16@gmail.com>
2022-06-27 18:01:09 +0200

    Add imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,7 @@
 #include "game.hpp"
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 
 int main() {
     Game game;
@@ -7,9 +10,21 @@ int main() {
 
     Rect rect(0.0f, 0.0f, texture.width, texture.height);
 
+    ImGui::CreateContext();
+    ImGui_ImplGlfw_InitForOpenGL(game.window, true);
+    ImGui_ImplOpenGL3_Init("#version 130");
+
     while (!glfwWindowShouldClose(game.window)) {
         if (glfwGetKey(game.window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(game.window, true);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
 
         glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
@@ -23,9 +38,15 @@ int main() {
         game.render_rect_centered(glm::vec3(1, 0, 0), Rect(0, -1.5f, 130, 130),
                                   45.0f);
 
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
         glfwSwapBuffers(game.window);
         glfwPollEvents();
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
 
     return 0;
 }
